5a590f8fe98290af07bd0af96700eb4cce115ae5
Corama Yunus <coramayunus@gmail.com>
2020-04-10 00:23:29 +0300

    Separate styles for sections header/header/trending

--- public/javascript/allJs.js
+++ public/javascript/allJs.js
@@ -1,9 +1,27 @@
+const pathToLogo = '../images/';
+const logoFormat = '.jpg';
+
+var logo = {
+    image : 'silver'
+}
 var companyName = 'Smartfleek'
 
-document.querySelector('title').textContent = companyName;
+var title = document.querySelector('title');
+title.textContent = companyName;
+
+var head = document.querySelector('head');
+var imagelogo = document.createElement('link');
+imagelogo.setAttribute('rel', 'icon')
+imagelogo.setAttribute('type', 'image/jpg');
+imagelogo.setAttribute('href', pathToLogo +logo.image + logoFormat);
+
+head.appendChild(imagelogo);
+
 var allTitleTags = document.getElementsByClassName('title');
 for(let i =0; i < allTitleTags.length; i++){
     allTitleTags[i].textContent = companyName;
 }
 
 document.querySelector('.title-categories').textContent = companyName + ' market';
+
+
