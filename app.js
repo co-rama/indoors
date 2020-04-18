0ac8c51f5b22d612145126e9d2653458317c801e
Corama Yunus <coramayunus@gmail.com>
2020-04-19 01:46:03 +0300

    Set a mongoose object to accept params and routes for products and users"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+require('dotenv/config');
 
 // Set static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,6 +16,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 
+
+// Database connection 
+const options = {
+    useNewUrlParser: true ,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(process.env.DB_CONNECTION_URL, options, (err) => {
+    if(err) console.error(err);
+    else console.log('Connected to the DB');
+})
+
 /**
  * Routes
  */
@@ -27,11 +40,20 @@ const trendingPage = require('./routes/trending');
 app.use('/', trendingPage);
 
 // Random Page Route
-const randomPage = require('./routes/page');
-app.use('/', randomPage);
+// const randomPage = require('./routes/page');
+// app.use('/', randomPage);
+
+// Users Routes
+const userPage = require('./routes/users');
+app.use('/', userPage);
+
+// Product Page Routes
+const prouductPage = require('./routes/product');
+app.use('/',prouductPage);
+
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, ()=>{
-    console.log('Server up and running');
+    console.log('Server up and running at: ' + PORT);
 })
 
